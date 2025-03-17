2fb23bd25ff082f71496c5007ab024d5f4f3209c
Mario Cordero <mariogabriel.cordero@ucr.ac.cr>
2025-03-17 11:42:09 -0600

    - FEAT: Adding the controller class

--- Entregable #1/controller/Controller.py
+++ Entregable #1/controller/Controller.py
@@ -1,49 +1,17 @@
 from view.View import View
-from model.Model import Model
+# from model.Model import Model
 
 class Controller:
 
     #
-    # Constructor del Controller
+    # Constructor of Controller
     #
     def __init__(self):
         """
-        Inicializa la vista y el modelo.
+        Initialize the view
         """
-        self.view = View(self)  # Construye la vista y pasa la controladora por parámetro
-        self.model = Model(self)  # Construye el modelo y pasa la controladora por parámetro
-        print("Controladora construida!")
-
-    def updateTable(self):
-        """
-        Actualiza la tabla en la vista con los datos obtenidos del modelo.
-        """
-        projects = self.model.fetchData()  # Obtiene los datos de la base de datos
-        self.view.updateTable(projects)  # Actualiza la tabla en la vista
-
-
-    def addData(self):
-        """
-        Analiza el archivo seleccionado.
-        """
-        filePath = self.view.selectFile()
-        if  filePath == -1:
-            print("No se ha seleccionado ningún archivo para analizar.")
-        else:
-            print(f"Analizando y añadiendo proyectos del archivo: {filePath}")
-            self.model.addXLSX(filePath)
-
-    def trainModel(self):
-        """
-        # Entrena el modelo
-        """
-        self.model.trainModel() # Primero entrenar el modelo
-
-    def analyzeDB(self):
-        """
-        # usa el modelo entrenado para predecir etiquetas
-        """
-        self.model.analyzeData()
+        self.view = View(self)  # Build the view and pass the controller as a parameter
+        print("Controller build!")
 
     #
     # Controller logic
@@ -53,19 +21,4 @@ class Controller:
         # Inicia la vista.
         """
         self.view.run()
-        self.updateTable()  # Actualiza la tabla al iniciar
-        self.view.landingPage.pushButton.clicked.connect(
-            lambda: self.view.stackedWidget.setCurrentWidget(self.view.databaseExplorer) # Connect "Empezar" button to show the databaseExplorer page
-        )
-        self.view.databaseExplorer.addDataButton.clicked.connect(
-            self.addData # The add data button
-        )
-        self.view.databaseExplorer.analyzeProyectsButton.clicked.connect(
-            self.analyzeDB # The analize button
-        )
-        self.view.databaseExplorer.trainModelButton.clicked.connect(
-            self.trainModel # The analize button
-        )
-        self.view.databaseExplorer.clearDatabaseButton.clicked.connect(
-            self.model.clearDatabase  # Botón para borrar todas las entradas de la base de datos
-        )
+        
